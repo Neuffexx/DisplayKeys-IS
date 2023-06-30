ddf53f063020736ef91d727a7faa77589000c84d
Neuffexx <63469833+Neuffexx@users.noreply.github.com>
2023-06-30 23:44:48 +0300

    Updated GIF Support in DisplayKeys-IS.py
    1.) replaced method of application finding its packaged .ico 
       - this made import sys unused (was removed)
    2.) added support for GIF files
       - Automatically checks whether user selected file is of .gif format or a static image format (supported: .png, .jpg, .jpeg, .bmp)
       - Will attempt to preserve frame timing, otherwise replace with average if any frame times are already present
          - Alternatively will use default of 100 (*1/10th if a second)

--- ProjectFiles/DisplayKeys-IS.py
+++ ProjectFiles/DisplayKeys-IS.py
@@ -6,7 +6,6 @@
 
 from PIL import Image, ImageSequence
 import os
-import sys
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
@@ -17,31 +16,39 @@ from tkinter import filedialog, messagebox
 
 def get_supported_types():
     # The supported file formats:
-    sup_image_formats = [".png", ".jpg", ".jpeg", ".tiff"]
+    sup_image_formats = [".png", ".jpg", ".jpeg", ".bmp"]
     sup_animated_formats = [".gif"]
     return sup_image_formats, sup_animated_formats
 
 
 def determine_split_type(file_path, output_dir, rows, cols, gap_horizontal, gap_vertical):
+    print("Determening File Type")
     # The supported file formats:
     image_formats = get_supported_types()[0]
     animated_formats = get_supported_types()[1]
 
+    print("File Types are: ")
+    print(get_supported_types()[0])
+    print(get_supported_types()[1])
+
     try:
         # Check if image format is supported
         with Image.open(file_path) as image:
+            print("Image can be opened: " + ("True" if image else "False") + "\n   Image format is: " + "." + image.format.lower())
             # Is Image
-            if image.format.lower() in image_formats:
+            if "." + image.format.lower() in image_formats:
                 split_image(file_path, output_dir, rows, cols, gap_horizontal, gap_vertical)
                 return True
             # Is Animated
-            elif image.format.lower() in animated_formats:
+            elif "." + image.format.lower() in animated_formats:
                 split_gif(file_path, output_dir, rows, cols, gap_horizontal, gap_vertical)
                 return True
+            else:
+                print("No formats matched")
 
     # Is not of a supported image format
     except TypeError:
-        # In the future simply open a Pop-Up Window with an error message as such:
+        # In the future simply open a Pop-Up Window with an error message:
         # |----------------------------------------|
         # | File Format not supported \n           |
         # | Currently supported formats are: \n    |
@@ -49,6 +56,7 @@ def determine_split_type(file_path, output_dir, rows, cols, gap_horizontal, gap_
         # | - Animated  | get_supported_types()[1] |
         # |----------------------------------------|
         # Do nothing for now
+        print("Wrong File Type")
         return None
 
 
@@ -112,78 +120,107 @@ def split_image(image_path, output_dir, rows, cols, gap_horizontal, gap_vertical
 
 
 def split_gif(gif_path, output_dir, rows, cols, gap_horizontal, gap_vertical):
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
     # Open the image using PIL
     gif = Image.open(gif_path)
-
+    print("GIF Frame Count: " + str(gif.n_frames))
     # Extract frames from .gif file
     frames = []
     for frame in ImageSequence.Iterator(gif):
         frames.append(frame.copy())
 
+    # Get duration of each frame
+    frame_durations = []
+    for frame in range(0, gif.n_frames):
+        gif.seek(frame)
+        try:
+            duration = int(gif.info['duration'])
+            if duration > 0:
+                frame_durations.append(duration)
+            else:
+                frame_durations.append(0)
+        except(KeyError, TypeError):
+            print("No frame durations present")
+            # Add default time in case no frame duration is provided by .gif
+            frame_durations.append(0)
+    # Calculate average duration
+    non_zero_durations = [d for d in frame_durations if d > 0]
+    if len(non_zero_durations) > 0:
+        default_duration = sum(non_zero_durations) // len(non_zero_durations)
+    else:
+        default_duration = 100
+    # Replace missing values with average duration
+    for i in range(len(frame_durations)):
+        if frame_durations[i] == 0:
+            frame_durations[i] = default_duration
+    print("Frame Durations: \n" + frame_durations.__str__())
+
     # Calculate the width and height of each image-cell
     width, height = frames[0].size
     cell_width = (width - (cols - 1) * gap_horizontal) // cols
     cell_height = (height - (rows - 1) * gap_vertical) // rows
-
-    # Create the output directory if it doesn't exist
-    os.makedirs(output_dir, exist_ok=True)
-
     # Determine the maximum cell size (to maintain square format)
     max_cell_size = min(cell_width, cell_height)
-
     # Calculate the horizontal and vertical offsets for cropping
     horizontal_offset = (cell_width - max_cell_size) // 2
     vertical_offset = (cell_height - max_cell_size) // 2
-
     # Determine the longest dimension (width or height)
     longest_dimension = "width" if cell_width > cell_height else "height"
     
     # Split Frames
     modified_frames = []
-
     for row in range(rows):
         for col in range(cols):
+            # Calculate the coordinates for cropping
+            left = col * (cell_width + gap_horizontal) + horizontal_offset
+            upper = row * (cell_height + gap_vertical) + vertical_offset
+
+            # Remove rows/columns only if they are the outermost rows/columns
+            if row == 0:
+                upper += vertical_offset
+            elif row == rows - 1:
+                upper -= vertical_offset
+
+            if col == 0:
+                left += horizontal_offset
+            elif col == cols - 1:
+                left -= horizontal_offset
+
+            if longest_dimension == "width":
+                right = left + max_cell_size
+                lower = upper + cell_height
+            else:
+                right = left + cell_width
+                lower = upper + max_cell_size
+
             # Perform operation on each frame, for each row/column split image-cell
             for frame in frames:
-                # Calculate the coordinates for cropping
-                left = col * (cell_width + gap_horizontal) + horizontal_offset
-                upper = row * (cell_height + gap_vertical) + vertical_offset
 
-                # Remove rows/columns only if they are the outermost rows/columns
-                if row == 0:
-                    upper += vertical_offset
-                elif row == rows - 1:
-                    upper -= vertical_offset
-
-                if col == 0:
-                    left += horizontal_offset
-                elif col == cols - 1:
-                    left -= horizontal_offset
-
-                if longest_dimension == "width":
-                    right = left + max_cell_size
-                    lower = upper + cell_height
-                else:
-                    right = left + cell_width
-                    lower = upper + max_cell_size
-
-                # Crop the image-cell
+                # Crop the current frame
                 image_cell = frame.crop((left, upper, right, lower))
 
                 # Hold onto cropped image-cell
-                modified_frames.append(frame)
+                modified_frames.append(image_cell)
 
             # Generate the output file path
             output_path = os.path.join(output_dir, f"gif_cell_{row}_{col}.gif")
+
+            print("Num of Modified Frames: " + str(modified_frames.__sizeof__()))
 
             # Save all frames of the image-cell into a single .gif file
             modified_frames[0].save(
                 output_path,
                 save_all=True,
-                append_image=modified_frames[1:],
+                append_images=modified_frames[1:],
+                duration=frame_durations, # Might make this a user definable variable in the future
                 loop=0
             )
 
+            modified_frames = []
+
+            print(f"gif_cell_{row}_{col} has this many frames: " + str(Image.open(output_path).n_frames))
             print(f"Saved {output_path}")
 
 
@@ -198,7 +235,9 @@ class ImageSplitterGUI:
     def __init__(self, entries):
         #GUI window
         self.window = tk.Tk()
-        icon_path = sys._MEIPASS + "./DisplayKeys-IS.ico"
+        #icon_path = sys._MEIPASS + "./DisplayKeys-IS.ico"
+        app_dir = os.path.dirname(os.path.abspath(__file__))
+        icon_path = os.path.join(app_dir, "./assets/DisplayKeys-IS.ico")
         self.window.iconbitmap(icon_path)
         self.window.title("DisplayKeys-IS")
         self.window.geometry("300x500")
@@ -407,6 +446,7 @@ def process_image(entries):
     # Call the image splitting function
     determine_split_type(image_path, output_dir, rows, cols, gap_horizontal, gap_vertical)
 
+    print("Process Image Exit")
 
 ####################################################################################################################
 #                                                    Create GUI
